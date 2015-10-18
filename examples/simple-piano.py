53de0a14f4aed844948a0891fa1229b40897c701
Philip Howard <phil@gadgetoid.com>
2015-10-18 10:24:19 +0100

    Update simple-piano.py

--- examples/simple-piano.py
+++ examples/simple-piano.py
@@ -2,50 +2,66 @@
 
 import pianohat
 import pygame
-import time
 import signal
 import glob
 import os
 import re
+BANK = os.path.join(os.path.dirname(__file__), "sounds")
 
-BANK      = './sounds/'
-FILETYPES = ['*.wav','*.ogg']
-samples   = []
-files     = []
-octave    = 0
-octaves   = 0
+print("""
+This example gives you a simple, ready-to-play instrument which uses .wav files.
+
+For it to work, you must place directories of wav files in:
+
+{}
+
+We've supplied a piano and drums for you to get started with!
+
+Press CTRL+C to exit.
+""".format(BANK))
+
+FILETYPES = ['*.wav', '*.ogg']
+samples = []
+files = []
+octave = 0
+octaves = 0
 
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.mixer.init()
 pygame.mixer.set_num_channels(32)
 
-patches = glob.glob(os.path.join(BANK,'*'))
+patches = glob.glob(os.path.join(BANK, '*'))
 patch_index = 0
 
 if len(patches) == 0:
-    exit('You need some patches in {}'.format(BANK))
+    exit("Couldn't find any .wav files in: {}".format(BANK))
+
 
 def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
     return [int(text) if text.isdigit() else text.lower() for text in re.split(_nsre, s)]
+
 
 def load_samples(patch):
     global samples, files, octaves, octave
     files = []
     print('Loading Samples from: {}'.format(patch))
     for filetype in FILETYPES:
-        files.extend(glob.glob(os.path.join(patch,filetype)))
+        files.extend(glob.glob(os.path.join(patch, filetype)))
     files.sort(key=natural_sort_key)
     octaves = len(files) / 12
     samples = [pygame.mixer.Sound(sample) for sample in files]
-    octave = octaves/2
+    octave = octaves / 2
+
 
 pianohat.auto_leds(True)
 
+
 def handle_note(channel, pressed):
-    channel = channel + (12*octave)
+    channel = channel + (12 * octave)
     if channel < len(samples) and pressed:
         print('Playing Sound: {}'.format(files[channel]))
         samples[channel].play(loops=0)
+
 
 def handle_instrument(channel, pressed):
     global patch_index
@@ -55,17 +71,20 @@ def handle_instrument(channel, pressed):
         print('Selecting Patch: {}'.format(patches[patch_index]))
         load_samples(patches[patch_index])
 
+
 def handle_octave_up(channel, pressed):
     global octave
     if pressed and octave < octaves:
         octave += 1
         print('Selected Octave: {}'.format(octave))
 
+
 def handle_octave_down(channel, pressed):
     global octave
     if pressed and octave > 0:
         octave -= 1
         print('Selected Octave: {}'.format(octave))
+
 
 pianohat.on_note(handle_note)
 pianohat.on_octave_up(handle_octave_up)
