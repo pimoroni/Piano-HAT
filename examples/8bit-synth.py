75f3ef9b3f2b9e4a0409ba33569e59058f9e2a3c
Phil Howard <phil@gadgetoid.com>
2015-11-03 10:23:54 +0000

    New 8bit pure soft synth

---
+++ examples/8bit-synth.py
@@ -0,0 +1,71 @@
+import pianohat
+import pygame
+import numpy
+import time
+import math
+import signal
+print("""
+8-bit Piano HAT
+
+This advanced example demonstrates software synthesis.
+
+It uses pygame's sndarray.make_sound method to create
+8bit tones with specific frequency, bitrate and samplerates.
+
+The octave up/down and instrument buttons are not supported (yet).
+
+Please wait while Pygame is set up and samples are generates...
+
+Press CTRL+C to exit!
+""")
+
+BITRATE = 8
+SAMPLERATE = 11025
+
+def play_sample(channel, pressed):
+    if pressed:
+        notes[channel].play(loops=-1)
+    else:
+        notes[channel].stop()
+
+def generate_sample(frequency=440, duration=1, bit_rate=8, sample_rate=11025):
+    sample_count = int(round(duration*sample_rate))
+
+    buffer = numpy.zeros((sample_count, 2), dtype = numpy.int16)
+    max_sample = 2**(bit_rate-1) - 1
+
+    for s in range(sample_count):
+        t = float(s)/sample_rate # Time index
+
+        buffer[s][0] = int(round(max_sample*math.sin(2*math.pi*frequency*t)))
+        buffer[s][1] = buffer[s][0]
+
+    sound = pygame.sndarray.make_sound(buffer)
+
+    return sound
+
+print("Initializing subsystem...")
+pygame.mixer.pre_init(SAMPLERATE, -BITRATE, 2, 512)
+pygame.mixer.init()
+
+print("Generating samples...")
+notes = [generate_sample(frequency=f, duration=1) for f in [
+        261.626,
+        277.183,
+        293.665,
+        311.127,
+        329.628,
+        349.228,
+        369.994,
+        391.995,
+        415.305,
+        440.000,
+        466.164,
+        493.883,
+        523.251
+    ]]
+
+pianohat.on_note(play_sample)
+print("Ready...")
+
+signal.pause()
